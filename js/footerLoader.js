a69fd47d0ae9b8b159eedcffba6aec248c584003
jeffstevens <steven_j1@denison.edu>
2024-12-02 07:09:00 -0500

    Added metricool tracking

--- js/footerLoader.js
+++ js/footerLoader.js
@@ -5,3 +5,5 @@ $(document).ready(function() {
       console.log("Should have loaded the footer."); // Callback optional here...
     });
 });
+
+function loadScript(a){var b=document.getElementsByTagName("head")[0],c=document.createElement("script");c.type="text/javascript",c.src="https://tracker.metricool.com/resources/be.js",c.onreadystatechange=a,c.onload=a,b.appendChild(c)}loadScript(function(){beTracker.t({hash:"79d23b4dd05eccdecf0d980f85773ce1"})});
